c3956b5029766257b2d22e997a3af1767ef67799
AlexB <xkenthor@gmail.com>
2021-01-28 22:13:40 +0300

    Changed format.

--- tests/send_test.py
+++ tests/send_test.py
@@ -15,7 +15,14 @@ sock = socket.socket(socket.AF_INET,
                     socket.SOCK_DGRAM)
 
 while True:
-    msg = str(random.randint(0, 100)).encode()
+# for i in range(100):
+    msg = ""
+    for i in range(random.randint(1,5)):
+        msg += (str(random.randint(0, 100))+"&")
+
+    msg = msg[:-1]
+    msg = msg.encode()
+
     sock.sendto(msg, (ip, port))
 
-    time.sleep(3)
+    time.sleep(1)
