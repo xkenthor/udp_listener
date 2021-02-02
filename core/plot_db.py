a374353386129151e38b14783033f0539ef42837
AlexB <xkenthor@gmail.com>
2021-02-02 23:08:29 +0300

    Changed concatenation function, added new variables & methods to PlotDb class.

--- core/plot_db.py
+++ core/plot_db.py
@@ -98,7 +98,7 @@ def cvt_raw2plot(raw_data, split_symbol="&"):
         try:
             data_list.append((utime, float(value)))
         except Exception as error:
-            gu.log("[ERROR]: "+error.__str__())
+            gu.log(error.__str__(), 1)
 
     return data_list
 
@@ -113,7 +113,8 @@ def concatenate_2dplot_dot(plot_object, dot):
             "x_label": < str >,
             "y_label": < str >,
             "x": < list >,
-            "y": < list >
+            "y": < list >,
+            "backup_count": < int >
         }
     dot -- < tuple > of < int/float > & < int/float > x & y that will be
         concatenated in the following format: (x, y).
@@ -125,6 +126,7 @@ def concatenate_2dplot_dot(plot_object, dot):
 
     plot_object['x'].append(dot[0])
     plot_object['y'].append(dot[1])
+    plot_object['backup_count'] += 1 #len(dot[0])
 
 def concatenate_2dplot_dot_list(plot_object, dot_list):
     """
@@ -137,7 +139,8 @@ def concatenate_2dplot_dot_list(plot_object, dot_list):
             "x_label": < str >,
             "y_label": < str >,
             "x": < list >,
-            "y": < list >
+            "y": < list >,
+            "backup_count": < int >
         }
     dot_list -- < list > of < tuple > of < int/float > & < int/float > list of
         x & y that will be concatenated in the following format: [(x, y), ..].
@@ -148,7 +151,13 @@ def concatenate_2dplot_dot_list(plot_object, dot_list):
 
 class PlotDB:
 
-    def __init__(self, source_object, graphs_amount, dump_save_path='dumps'):
+    def __init__(self,
+                source_object,
+                graphs_amount,
+                dump_save_dir='dumps',
+                dumping_enabled=True,
+                default_plot_backup=_default_plot_backup,
+                default_plot_thresh=_default_plot_thresh):
         """
         This class implements I/O methods, raw data processing received by
             source object, provides external access.
@@ -156,37 +165,33 @@ class PlotDB:
         Keyword arguments:
         source_object -- < any > object from which this class will read plot
             values. Data source object must have .get_data() method.
-        source_object -- < int > amount of graphs that will be
+        graphs_amount -- < int > number of graphs that will be created.
+        dump_save_dir -- < str > path to dump directory where graphs will be
+            saved.
+        dumping_enabled --  < bool > True/False, enabled/disabled.
+        default_plot_backup -- < int > backup count value.
+        default_plot_thresh -- < int > threash value.
 
         """
-        if source_object is None:
-            raise TypeError('Specified object is None type.')
-
         if graphs_amount < 1:
             raise IndexError('Specified number of graphs less than 1.')
 
-        if os.path.exists(dump_save_path) != True:
-            if dump_save_path == 'dumps':
-                os.mkdir('dumps')
-            else:
-                raise FileNotFoundError(
-                                    'Specified dump save path does not exist')
-
-        self.__source_object = source_object
-        self.__graph_amount = graphs_amount
-        self.__plot_list = []
         self.__on_process = False
         self.__process_thread = None
 
-        self.__dump_save_path = dump_save_path
+        self.__graph_amount = graphs_amount
+        self.__plot_list = []
+
+        self.set_source_object(source_object)
+        self.set_dump_save_dir(dump_save_dir)
+        self.set_dumping_enable_flag(dumping_enabled)
+        self.set_plot_backup(default_plot_backup)
+        self.set_plot_thresh(default_plot_thresh)
 
         for i in range(self.__graph_amount):
             new_plot = generate_2dplot_dict(_template_graph_name.format(i))
             new_plot.update({"backup_count": 0})
             self.__plot_list.append(new_plot)
-
-        self.__thresh_count = _default_plot_thresh
-        self.__backup_count = _default_plot_backup
 
     def __check_status(self):
         """
@@ -198,14 +203,14 @@ class PlotDB:
 
         """
         if self.__process_thread is not None:
-            gu.log("[ERROR]: Processing thread is still working. If you want"+\
-                                " to restart call stop_processing() first.")
+            gu.log("Processing thread is still working. If you want"+\
+                                " to restart call stop_processing() first.", 1)
 
             return True
 
         if self.__on_process:
-            gu.log("[ERROR]: Processing is currently working. If you want"+\
-                                " to restart call stop_processing() first.")
+            gu.log("Processing is currently working. If you want"+\
+                                " to restart call stop_processing() first.", 1)
 
             return True
 
@@ -224,8 +229,8 @@ class PlotDB:
             received_data = cvt_raw2plot(received_data)
 
             if len(received_data) != self.__graph_amount:
-                gu.log('[ERROR]: Received data does not match the number ' +\
-                                        'of graphs. Received data ignored.')
+                gu.log('Received data does not match the number ' +\
+                                        'of graphs. Received data ignored.', 1)
                 continue
 
             for index in range(self.__graph_amount):
@@ -235,8 +240,12 @@ class PlotDB:
             self.__crop_plot_object_list()
 
             for plot_object in self.__plot_list:
+
                 if plot_object['backup_count'] >= self.__backup_count:
-                    self.dump_plot_object(plot_object)
+
+                    if self.__dumping_enabled:
+                        self.dump_plot_object(plot_object)
+
                     plot_object['backup_count'] = 0
 
     def __crop_plot_object_list(self):
@@ -273,7 +282,7 @@ class PlotDB:
             plot_object['x'] = plot_object['x'][difference:]
             plot_object['y'] = plot_object['y'][difference:]
 
-            plot_object['backup_count'] += difference
+            # plot_object['backup_count'] += difference
 
     def dump_plot_object(self, plot_object):
         """
@@ -292,17 +301,18 @@ class PlotDB:
 
         """
         object_to_save = plot_object.copy()
+        object_to_save['x'] = object_to_save['x'].copy()[-self.__backup_count:]
+        object_to_save['y'] = object_to_save['y'].copy()[-self.__backup_count:]
         object_to_save.pop("backup_count")
 
         utime_start = datetime.datetime.utcfromtimestamp(
                                                     object_to_save['x'][0])
-
         name = object_to_save['name']
         date = utime_start.strftime(_template_date_name)
         x_start_time = utime_start.strftime(_template_time_name)
         duration = str(int(object_to_save['x'][-1] - object_to_save['x'][0]))
 
-        save_path = os.path.join(self.__dump_save_path, name)
+        save_path = os.path.join(self.__dump_save_dir, name)
 
         if os.path.exists(save_path) != True:
             os.makedirs(save_path)
@@ -317,6 +327,37 @@ class PlotDB:
 
         gu.write_gzip(save_path, object_to_save, compresslevel=9)
 
+    def set_dump_save_dir(self, dump_save_dir):
+        """
+        This method changes dump path.
+
+        Keyword arguments:
+        dump_save_dir -- < str > new dump path.
+
+        """
+        if os.path.exists(dump_save_dir) != True or \
+                                    os.path.isfile(dump_save_dir) == True:
+
+            if os.path.split(dump_save_dir)[0] != '':
+                raise FileNotFoundError(
+                    'Specified dump save path does not exist or corrupted.')
+
+            os.makedirs(dump_save_dir)
+        self.__dump_save_dir = dump_save_dir
+
+    def set_dumping_enable_flag(self, flag):
+        """
+        This method changes dumping_enabled variable.
+
+        Keyword arguments:
+        flag -- < bool > True/False, enabled/disabled.
+
+        """
+        if type(flag) is not bool:
+            raise TypeError('Specified flag is not bool type.')
+
+        self.__dumping_enabled = flag
+
     def set_source_object(self, source_object):
         """
         This method closes connection with old source_object, drops it and
@@ -327,6 +368,9 @@ class PlotDB:
             values. Data source object must have .get_data() method.
 
         """
+        if source_object is None:
+            raise TypeError('Specified object is None type.')
+
         on_process = False
         s_thread = False
 
@@ -336,7 +380,8 @@ class PlotDB:
             if self.__process_thread is not None:
                 s_thread = True
 
-        self.stop_processing()
+            self.stop_processing()
+
         self.__source_object = source_object
 
         if on_process:
@@ -360,14 +405,18 @@ class PlotDB:
 
     def set_plot_thresh(self, thresh_count):
         """
-        This method changes the value required for the cropping.
+        This method changes the value required for the cropping. This value
+            must be more than backup count, otherwise last one won't work.
 
         Keyword arguments:
-        backup_count -- < int > new count value.
+        thresh_count -- < int > new count value.
 
         """
         if type(thresh_count) is not int:
-            raise TypeError('New count value is not of type int.')
+            raise TypeError('New thresh value is not of type int.')
+
+        if thresh_count < self.__backup_count:
+            raise ValueError('New thresh value is less than backup count.')
 
         self.__thresh_count = thresh_count
 
